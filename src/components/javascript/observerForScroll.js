fda20b10d4fe1bd1c12f8dbfc6b3d35ed5e062b4
HillTrigger <mslava32@gmail.com>
2024-12-24 16:47:31 +0600

    добавил анимацию при скролле

--- src/components/javascript/observerForScroll.js
+++ src/components/javascript/observerForScroll.js
@@ -19,17 +19,38 @@ function onEntry(entry) {
             return;
         }
       }
+      if (element.classList.contains("strategies__title")) {
+        element
+          .querySelector(".strategies__line")
+          .classList.add("element-show");
+      }
+      if (element.classList.contains("populars__main-title")) {
+        element.querySelector(".populars__line").classList.add("element-show");
+      }
+      if (element.classList.contains("forecast__card-title")) {
+        element.querySelector(".populars__line").classList.add("element-show");
+      }
     }
   });
-}
-
+} // populars__line
 let options = {
-  threshold: [0.5],
+  threshold: [0.3],
 };
 let observer = new IntersectionObserver(onEntry, options);
 
-let elements = [...document.querySelectorAll(".stats__stat")];
-
+let elements = [
+  ...document.querySelectorAll(".stats__stat"),
+  document.querySelector(".strategies__title"),
+  ...document.querySelectorAll(".strategies__card"),
+  document.querySelector(".populars__main-title"),
+  ...document.querySelectorAll(".populars__card"),
+  document.querySelector(".populars__all-products"),
+  document.querySelector(".forecast__top"),
+  ...document.querySelectorAll(".forecast__card"),
+  document.querySelector(".main__subscribe"),
+  document.querySelector(".main__question"),
+];
+console.log(elements);
 for (let elm of elements) {
   observer.observe(elm);
 }
